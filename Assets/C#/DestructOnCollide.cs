c520e9bf0b110f14ba90c77c7a4b8201d09e962b
Tartiflexe <ponthieuleonard@gmail.com>
2018-02-23 16:24:20 +0100

    Add physics to building

--- Assets/C#/DestructOnCollide.cs
+++ Assets/C#/DestructOnCollide.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class DestructOnCollide : MonoBehaviour {
 
-	
+    Rigidbody rb;
 	void Start () {
-		
+        rb = GetComponent<Rigidbody>();
 	}
-	
-	
-	void Update () {
-		
-	}
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Avion")
+        {
+            rb.useGravity = true;
+            rb.isKinematic = false;
+        }
+    }
 }
